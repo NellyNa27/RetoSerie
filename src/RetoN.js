ec6c2ab3a75bca4ed2210b7240a9aff02508ad8a
nellyNa <nlly.rro@gmail.com>
2024-02-17 12:10:46 -0600

    se hace la primera formula para conocer un numero primo en seriePrimo

--- src/RetoN.js
+++ src/RetoN.js
@@ -1,24 +1,40 @@
 //Desarrollar una "clase" con un método que entregue el término n de la serie.
 class RetoSerie {
     // Realizar cada una de las Series numéricas
-    // Serie 1: Número primo
+    // Serie 1: Número primo - true
     // El número primo es un núm mayor que 1 
-    //El número primo tiene dos divisores: 1 y él mismo
+    //El número primo tiene dos divisores: 1 y él mismo (ejemplo: 2-3-5)
         seriePrimo(valorNumero) {
-            if (valorNumero >= 1)
-            return false;
-        
-            if (valorNumero % i === 0) {
-                return false;
+            if (valorNumero <= 0) return false;
+            //creamos variable para que nos diga cuantas veces divimos el numero
+            let contador = 0;
+            for (var i = 1; i <= valorNumero ; i++) {
+                //división exacta
+                if (valorNumero % i === 0) valorNumero++;
             }
-        
-        return true;
+            if (valorNumero === 2) return true;
+                    return false;
+            }
     }
-        }
-    
+
+    console.log ("Número 1= " + seriePrimo(1));
+    console.log ("Número 1= " + seriePrimo(2));
+    console.log ("Número 1= " + seriePrimo(3));
+    console.log ("Número 1= " + seriePrimo(4));
+    console.log ("Número 1= " + seriePrimo(5));
+    console.log ("Número 1= " + seriePrimo(6));
+    console.log ("Número 1= " + seriePrimo(7));
+    console.log ("Número 1= " + seriePrimo(8));
+    console.log ("Número 1= " + seriePrimo(9));    
+    console.log ("Número 1= " + seriePrimo(10));
+
+        
+    /*
     // Serie 2: Número Fibonacci
     // Recursividad
-        serieFibonacci() {}
+        serieFibonacci() {
+
+        }
         
     // Serie 3: Número Triangular
         serieTriangular(n) {
@@ -40,4 +56,4 @@ class RetoSerie {
         instancia.calcularNSerie();
     
     
-    
+    */
